7364b17396853c4feb29f61f822e6290a56c21b8
Allison Jin <allisonjin@Allisons-MacBook-Pro.local>
2017-09-21 18:50:25 -0700

    delete useless lines

--- app/src/main/java/edu/neu/madcourse/zhiyaojin/DisplayAboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/zhiyaojin/DisplayAboutActivity.java
@@ -4,13 +4,10 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
-
-import java.util.UUID;
 
 public class DisplayAboutActivity extends AppCompatActivity {
 
@@ -35,14 +32,6 @@ public class DisplayAboutActivity extends AppCompatActivity {
         }
 
         TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-//        String imei = telephonyManager.getImei();
-//        if (imei != null) {
-//            return imei;
-//        }
-//        String meid = telephonyManager.getMeid();
-//        if (meid != null) {
-//            return meid;
-//        }
         String uuid = telephonyManager.getDeviceId();
         if (uuid != null) {
             return uuid;
