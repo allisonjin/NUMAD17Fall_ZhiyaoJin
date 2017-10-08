1147e9756142b8a4b351ee06c06dd5e022058a73
Allison Jin <allisonjin@Allisons-MacBook-Pro.local>
2017-10-07 21:34:25 -0700

    basic game layout

---
+++ app/src/main/java/edu/neu/madcourse/zhiyaojin/activity/ScroggleActivity.java
@@ -0,0 +1,15 @@
+package edu.neu.madcourse.zhiyaojin.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.neu.madcourse.zhiyaojin.R;
+
+public class ScroggleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroggle);
+    }
+}
